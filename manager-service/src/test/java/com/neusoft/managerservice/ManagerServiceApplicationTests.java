6562e0dc7e1f0ae842a3b4088ce64f6ed4febb65
872499002 <jiangbiaofeng@qq.com>
2019-05-07 15:53:51 +0800

    201905  谷粒商城

--- manager-service/src/test/java/com/neusoft/managerservice/ManagerServiceApplicationTests.java
+++ manager-service/src/test/java/com/neusoft/managerservice/ManagerServiceApplicationTests.java
@@ -1,23 +1,60 @@
 package com.neusoft.managerservice;
 
 
-import com.neusoft.interfaces.SpuService;
-import com.neusoft.javabean.po.SpuSaleAttr;
-import com.neusoft.managerservice.serviceimpl.SpuServiceImpl;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSONObject;
+import com.neusoft.interfaces.AttrService;
+
+import com.neusoft.javabean.po.BaseCatalog1;
+import com.neusoft.javabean.po.BaseCatalog2;
+import com.neusoft.managerservice.serviceimpl.AttrServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ManagerServiceApplicationTests {
 
+    @Reference
+    AttrService attrService;
+
+
+    @Test
+    public void getCatalogJSON() throws IOException {
+        List<BaseCatalog1> baseCatalog1s = attrService.selectAllCatalog();
+        JSONObject jsonObject = new JSONObject();
+        HashMap<String, List<BaseCatalog2>> map = new HashMap<>();
+        for (BaseCatalog1 baseCatalog1 : baseCatalog1s) {
+            Long catalog1Id = baseCatalog1.getId();
+            List<BaseCatalog2> catalog2List = baseCatalog1.getCatalog2List();
+            jsonObject.put(catalog1Id.toString(),catalog2List);
+        }
+        String jsonString = jsonObject.toJSONString();
+        File file = new File("D:\\IDEA_Project\\guli201904\\manager-service\\src\\main\\resources\\static\\catalog.json");
+
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        FileOutputStream stream = new FileOutputStream(file);
+        stream.write(jsonString.getBytes("utf-8"));
+        stream.close();
+    }
+
+
     @Test
     public void contextLoads() {
-
-}
-
+    }
 }
--- order-web/src/main/java/com/neusoft/orderweb/controller/OrderController.java
+++ order-web/src/main/java/com/neusoft/orderweb/controller/OrderController.java
@@ -1,16 +1,20 @@
 package com.neusoft.orderweb.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.neusoft.interfaces.CartService;
 import com.neusoft.interfaces.OrderService;
 import com.neusoft.interfaces.UserService;
-import com.neusoft.javabean.po.OrderInfo;
-import com.neusoft.javabean.po.UserInfo;
+import com.neusoft.javabean.po.*;
+import com.neusoft.webutils.annotation.LoginRequire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,18 +24,47 @@ public class OrderController {
 
     @Reference
     private UserService userService;
-    @RequestMapping("/insert.do")
-    public String insertOrder(ModelMap map){
-        List<OrderInfo> orderInfos = orderService.selectAllOrderInfo();
-        for(OrderInfo orderInfo : orderInfos){
-            System.out.println(orderInfo);
+
+    @Reference
+    CartService cartService;
+
+
+    @LoginRequire(ifNeedSuccess = true)
+    @RequestMapping("toTrade")
+    public String toTrade(HttpServletRequest request, ModelMap map){
+        String userId = (String)request.getAttribute("userId");
+        //将被选中的购物车对象转化为订单对象，展示出来
+        List<CartInfo> cartInfos = cartService.getCatCacheByChecked(userId);
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        for (CartInfo cartInfo : cartInfos) {
+            OrderDetail orderDetail = new OrderDetail();
+            //将购物车对象转化为订单对象
+            orderDetail.setImgUrl(cartInfo.getImgUrl());
+            orderDetail.setOrderPrice(cartInfo.getCartPrice());
+            orderDetail.setSkuId(cartInfo.getSkuId());
+            orderDetail.setSkuName(cartInfo.getSkuName());
+            orderDetail.setSkuNum(cartInfo.getSkuNum().toString());
+            orderDetailList.add(orderDetail);
         }
-        List<UserInfo> userInfos = userService.selectAllUserInfo();
-        for(UserInfo userInfo : userInfos){
-            System.out.println(userInfo);
-        }
-        map.put("orderInfos",orderInfos);
-        return "index";
+
+        //查询用户收货得至列表，让用户选择
+        List<UserAddress>  userAddressList = userService.getUserAddressList(userId);
+
+        map.put("userAddressList",userAddressList);
+        map.put("orderDetailList",orderDetailList);
+        //总金额
+        map.put("totalAmount",gettotalAmount(cartInfos));
+
+        return "trade";
     }
+
+    private BigDecimal gettotalAmount(List<CartInfo> cartInfos) {
+        BigDecimal b = new BigDecimal("0");
+        for (CartInfo cartInfo : cartInfos) {
+            b = b.add(cartInfo.getCartPrice());
+        }
+        return b;
+    }
+
 
 }
